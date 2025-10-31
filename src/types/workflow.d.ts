5b49c43ef65130ac4d360445e8ed12fcd88f3d13
glu <glucrypto@protonmail.com>
2025-10-31 12:48:33 -0400

    fix: made workflow optional and bump version (#63)

---
+++ src/types/workflow.d.ts
@@ -0,0 +1,5 @@
+declare module "workflow";
+declare module "workflow/internal/builtins";
+declare module "workflow/internal/private";
+declare module "workflow/runtime";
+declare module "workflow/api";
