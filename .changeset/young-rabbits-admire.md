17a60c18ddaee0ad61567cad8760543cf463878b
glu <glucrypto@protonmail.com>
2025-12-03 16:50:07 -0500

    Feat/clean examples (#92)
    * fix: removed workflow example
    
    * fix: removed stale workflow example

---
+++ .changeset/young-rabbits-admire.md
@@ -0,0 +1,5 @@
+---
+"opentool": patch
+---
+
+removed workflow example
--- examples/workflow-basic/metadata.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  name: "workflow-basic",
-  description: "Example project combining classic tools and Workflow DevKit directives.",
-};
